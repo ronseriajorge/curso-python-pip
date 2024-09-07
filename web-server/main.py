324daef7d5bb7c8b000ae2ace838651e573b7977
ronseriajorge <ronseriajorge@gmail.com>
2024-09-06 20:32:34 -0500

    Se crea una API con fastapi

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,23,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+    <h1>Hola soy platzi</h1>
+    <p>Hola soy un parrafo</p>
+    """
 
 def run():
     store.get_categories()
